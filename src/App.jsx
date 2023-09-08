a29afa9d9fc149e5590f6ee168c48280e7126a9a
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-08 17:42:30 +0330

    setup styled compoenents

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { styled } from "styled-components"
+
+const H1 = styled.h1`
+font-size : 50px;
+font-weight:600;
+`;
+const Button = styled.button`
+font-size: 1.4rem;
+padding: 1.2rem 1.6rem;
+font-weight: 5000;
+border: none;
+border-radius: 7px ;
+background-color: purple;
+color: white;
+`;
+
+
 function App() {
     return (
-        <div>
-            test
-        </div>
+        <>
+            <H1>
+                test styled components
+            </H1>
+            <Button>Check In</Button>
+        </>
     )
 }
 
 export default App
+
+
+
