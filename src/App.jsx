153cacc421a6c933aade59eb4ae71254e2545316
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-08 18:51:07 +0330

    set default props for ui compoenents

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,38 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+import { styled } from "styled-components";
+
+
+const StyledApp = styled.main`
+padding:20px;
+`;
 
 
 function App() {
     return (
         <>
             <GlobalStyles />
-            <Heading as='h1'>test styled components</Heading>
-            <Heading as='h2'>test styled components</Heading>
-            <Heading as='h3'>test styled components</Heading>
-            <Button>Check In</Button>
-            <Input></Input>
+            <StyledApp>
+                <Row type='vertical'>
+                    <Row type='horizontal'>
+                        <Heading as='h1'>The Wild Oasis</Heading>
+                        <div>
+                            <Heading as='h2'>Check in and out</Heading>
+                            <Button>Check In</Button>
+                            <Button>Check out</Button>
+                        </div>
+                    </Row>
+                    <Row type='vertical'>
+                        <Heading as='h3'>Form</Heading>
+                        <form>
+                            <Input type="number" placeholder="Number of guests" />
+                            <Input type="number" placeholder="Number of guests" />
+                        </form>
+                    </Row>
+                </Row>
+            </StyledApp>
         </>
     )
 }
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -4,20 +4,21 @@ const Heading = styled.h1`
 ${props => props.as === 'h1' && css`
 font-size : 3rem;
 font-weight:600;
-background-color: yellow;`}
+`}
 
 
 ${props => props.as === 'h2' && css`
 font-size : 2rem;
 font-weight:600;
-background-color: yellow;`}
+`}
 
 
 ${props => props.as === 'h3' && css`
 font-size : 2rem;
 font-weight:500;
-background-color: yellow;`}
+`}
 
+line-height:1.2;
 `;
 
 export default Heading
