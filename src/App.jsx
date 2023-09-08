cb0c2be11efcd66ce64a435d1567298b54ab87f6
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-08 17:59:39 +0330

    connect global styles with other compoenents

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ const H1 = styled.h1`
 font-size : 50px;
 font-weight:600;
 `;
+
+
 const Button = styled.button`
 font-size: 1.4rem;
 padding: 1.2rem 1.6rem;
 font-weight: 5000;
 border: none;
 border-radius: 7px ;
-background-color: purple;
+background-color: var(--color-brand-500);
 color: white;
 `;
 
