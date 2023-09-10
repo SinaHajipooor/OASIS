0f14f19a4c06ad5bab04cf9eff9479c42c6b3c32
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-10 10:06:17 +0330

    update settings form

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,24 +1,31 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSettings } from './useSettings';
+import Spinner from '../../ui/Spinner'
+
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+    // get data from our custom hook ( we are destructuring the settings object right here) // set  the settings object into empty array as a initial value to not get error if there wasnt any value 
+    const { isLoading, settings: { minBookingLength, maxBookingLength, maxGuestsPerBooking, breakfastPrice } = {} } = useSettings()
+    // ui
+    if (isLoading) return <Spinner />
+    return (
+        <Form>
+            <FormRow label='Minimum nights/booking'>
+                <Input type='number' id='min-nights' defaultValue={minBookingLength} />
+            </FormRow>
+            <FormRow label='Maximum nights/booking'>
+                <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
+            </FormRow>
+            <FormRow label='Maximum guests/booking'>
+                <Input type='number' id='max-guests' defaultValue={maxGuestsPerBooking} />
+            </FormRow>
+            <FormRow label='Breakfast price'>
+                <Input type='number' id='breakfast-price' defaultValue={breakfastPrice} />
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;
