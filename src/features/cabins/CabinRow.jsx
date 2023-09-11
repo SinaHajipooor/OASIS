fc91fbbe491dd1c5a86b641f3be86853d44a4331
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-11 18:36:13 +0330

    add base menu

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,6 +7,7 @@ import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from '../../ui/ConfirmDelete'
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 
 
@@ -61,7 +62,6 @@ function CabinRow({ cabin }) {
     }
     //ui 
     return (
-
         <Table.Row>
             <Img src={image} />
             <Cabin>{name}</Cabin>
@@ -84,10 +84,16 @@ function CabinRow({ cabin }) {
                         <ConfirmDelete resourceName='cabins' disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)} />
                     </Modal.Window>
                 </Modal>
+                <Menus.Menu>
+                    <Menus.Toggle id={cabinId} />
+                    <Menus.List id={cabinId}>
+                        <Menus.Button>Duplicate</Menus.Button>
+                        <Menus.Button>Edit</Menus.Button>
+                        <Menus.Button>Delete</Menus.Button>
+                    </Menus.List>
+                </Menus.Menu>
             </div>
         </Table.Row>
-
-
     )
 }
 
